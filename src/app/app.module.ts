a37e5d1300e94eaae17a6384cdfbe0d2bb983efe
Iryna <gresrin@gmail.com>
2023-03-28 21:24:27 +0200

    Create a new app template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
-import { AddTaskComponent } from './add-task/add-task.component';
 import {TaskService} from "./task.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent,
-    AddTaskComponent
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { TaskService } from '../task.service';
-import { Todo } from '../todo';
+
 
 @Component({
   selector: 'app-todo-list',
@@ -9,12 +9,15 @@ import { Todo } from '../todo';
 })
 export class TodoListComponent implements OnInit {
 
-  tasks: Todo[] = [];
+  tasks = [
+    { name: 'Task 1', completed: false },
+    { name: 'Task 2', completed: true },
+    { name: 'Task 3', completed: false }
+  ];
 
   constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
-    this.tasks = this.taskService.getTasks();
   }
 
 }
